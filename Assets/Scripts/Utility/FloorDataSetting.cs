97b70d999e9279f1b21b01b073998033b1c3a3fb
小仔 <71455438+chris58530@users.noreply.github.com>
2025-04-03 00:42:05 +0800

    測試

---
+++ Assets/Scripts/Utility/FloorDataSetting.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class FloorDataSetting : ScriptableObject
+{
+    public int floorNumber;
+    public string floorName;
+    public string floorDescription;
+    public GameObject floorPrefab;
+    public GameObject floorModel;
+    public GameObject floorUI;
+    public GameObject floorEffect;
+    public GameObject floorSound;
+}
+
